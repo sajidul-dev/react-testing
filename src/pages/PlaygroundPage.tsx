f9a422b5f0e599bae4ffb8163b7ed357e271fe3b
sajidul-dev <mdsajidulalam1245@gmail.com>
2024-10-28 21:49:34 +0600

    Lesson six: Test more user interaction with expandable text

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,8 +1,11 @@
 // import Onboarding from "../components/Onboarding";
-import TermsAndConditions from "../components/TermsAndConditions";
+import ExpandableText from "../components/ExpandableText";
+// import TermsAndConditions from "../components/TermsAndConditions";
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />;
+  return (
+    <ExpandableText text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quasi aliquid repellat sint magni mollitia obcaecati dolor aspernatur molestiae tenetur nesciunt, dolorem architecto reprehenderit optio culpa aut praesentium ratione voluptas autem itaque quis exercitationem earum? Facere harum laudantium inventore id error, nihil, maxime blanditiis numquam dicta animi quibusdam at praesentium voluptatem quasi excepturi. Odit totam molestias quae, aliquid eaque in reiciendis rerum aliquam! Fugiat fugit ad sit rem necessitatibus et debitis sunt, aspernatur aut corrupti vel maiores officia vero, neque delectus recusandae dolorum commodi? Rem quisquam est facilis ipsa cum incidunt, at, nostrum eligendi, deserunt eveniet debitis. Iste, sit nam!" />
+  );
 };
 
 export default PlaygroundPage;
