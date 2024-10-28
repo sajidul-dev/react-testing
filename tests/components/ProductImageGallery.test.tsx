0c63459e6b74d448dbb40783b723997bd332b018
sajidul-dev <mdsajidulalam1245@gmail.com>
2024-10-28 20:51:59 +0600

    Lesson four: Test empty dom element with Product image gallery

---
+++ tests/components/ProductImageGallery.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import ProductImageGallery from "../../src/components/ProductImageGallery";
+
+describe("ProductImageGallery", () => {
+  it("should render nothing if given an empty array", () => {
+    const { container } = render(<ProductImageGallery imageUrls={[]} />);
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it("should render a list of images", () => {
+    const imageUrls = [
+      "https://example.com/image1.jpg",
+      "https://example.com/image2.jpg",
+      "https://example.com/image3.jpg",
+    ];
+    render(<ProductImageGallery imageUrls={imageUrls} />);
+    const images = screen.getAllByRole("img");
+    expect(images).toHaveLength(3);
+    imageUrls.forEach((url, index) => {
+      expect(images[index]).toHaveAttribute("src", url);
+    });
+  });
+});
